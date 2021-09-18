755a64dff8b1b6b4e5273f4efb8b28e7c9d38b06
root <root@ip-172-31-3-70.ap-south-1.compute.internal>
2021-09-18 14:13:29 +0000

    one ruby file added for linux command execution with help of recipe

---
+++ rubywithlinux.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook:: test-cookbook
+# Recipe:: rubywithlinux
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+execute "run a script" do
+command <<-EOH
+mkdir /shanvi
+touch /shanvitext
+EOH
+end 
